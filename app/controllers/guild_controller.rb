5b1e528491532fb2ee76b307bd9721ff4afb5087
Rachel Kawula <rkawula@gmail.com>
2015-06-23 22:39:06 -0700

    fixed up navbar some more!

--- app/controllers/guild_controller.rb
+++ app/controllers/guild_controller.rb
@@ -5,8 +5,24 @@ class GuildController < ApplicationController
   	# show static view
   end
 
+  def blog
+
+  end
+
+  def events
+
+  end
+
+  def forum
+
+  end
+
   def history
   	# show static view
+  end
+
+  def apply
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   root 'guild#index'
 
-  # Static pages.
+  get 'blog', to: 'guild#blog'
+  get 'events', to: 'guild#events'
+  get 'forum', to: 'guild#forum'
   get 'history', to: 'guild#history'
+  get 'apply', to: 'guild#apply'
 
 end
