baf63cfed43d8d9eb4aaf49613cc12dd393ab2aa
Rachel Kawula <rkawula@gmail.com>
2015-07-29 16:38:52 -0700

    More work on user model.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,7 @@ class SessionsController < ApplicationController
   def create
   	# render text: request.env['omniauth.auth'].to_yaml
     auth = request.env["omniauth.auth"]
-    user = User.where(:provider => auth['provider'],
-    				  :uid => auth['uid'].to_s || User.create_with_omniauth(auth))
+    user = User.find_or_create(auth)
 	reset_session
 	session[:user_id] = user.uid
 	redirect_to root_url, :notice => 'Signed in!'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ActiveRecord::Base
 
-  def self.create_with_omniauth(auth)
-    create! do |user|
-      user.provider = auth['provider']
-      user.uid = auth['uid'].to_s
-      if auth['info']
-         user.name = auth['info']['name'] || ""
+  def self.find_or_create(auth)
+    user = User.where(:provider => auth['provider'], :uid => auth['uid'].to_s)
+    unless user
+      User.create! do
+        user.provider = auth['provider']
+        user.uid = auth['uid'].to_s
       end
     end
+    return user
   end
 
 end
