ef4ca58afb9f0b30c4eee756d7548637d3fd374b
Rachel Kawula <rkawula@gmail.com>
2015-07-29 14:50:32 -0700

    SSL was the solution.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	render text: request.env['omniauth.auth'].to_yaml
-    # auth = request.env["omniauth.auth"]
-    #user = User.where(:provider => auth['provider'],
-    #					:uid => auth['uid'].to_s.first || User.create_with_omniauth(auth))
-	#reset_session
-	#session[:user_id] = user.id
-	#redirect_to root_url, :notice => 'Signed in!'
+  	# render text: request.env['omniauth.auth'].to_yaml
+    auth = request.env["omniauth.auth"]
+    user = User.where(:provider => auth['provider'],
+    					:uid => auth['uid'].to_s.first || User.create_with_omniauth(auth))
+	reset_session
+	session[:user_id] = user.id
+	redirect_to root_url, :notice => 'Signed in!'
   end
 
   def destroy
