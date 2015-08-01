a7b240b3ff0fc6772e71e71f3f1fc6b5b8abfe68
Rachel Kawula <rkawula@gmail.com>
2015-07-31 21:23:14 -0700

    Another update to match the way I did this in a previous project.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ActiveRecord::Base
 
   def self.find_or_create(auth)
-    @user = find_by(provider: auth['provider'], uid: auth['uid']).first
-    unless @user
-      @user = User.create! provider: auth['provider'],
-                           name: auth['info']['name'],
-                           uid: auth['uid'].to_s
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
     end
-    return @user
   end
 
 end
