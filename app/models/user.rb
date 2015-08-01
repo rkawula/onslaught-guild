f78aa1cb94f482057e95404d62feef314d1f8c13
Rachel Kawula <rkawula@gmail.com>
2015-07-31 21:26:16 -0700

    Hopefully final change? To match Rails 4.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
   def self.find_or_create(auth)
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
