c7a0f999e8b540bec4cfa8b150cd35c6937b8bd3
Rachel Kawula <rkawula@gmail.com>
2015-07-31 21:48:39 -0700

    Removed comments! Should work!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,17 +8,17 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     @user = User.find_or_create(auth)
 	reset_session
-	session[:user_id] = @user.uid
-	#redirect_to root_url, :notice => 'Signed in!'
+	session[:user_id] = @user.id
+	redirect_to root_url, :notice => 'Signed in!'
   end
 
   def destroy
-    #reset_session
-    #redirect_to root_url, :notice => "Signed out!"
+    reset_session
+    redirect_to root_url, :notice => "Signed out!"
   end
 
   def failure
-  	#redirect_to root_url, :alert => "Authentication error: #{params[:message].humanize}"
+  	redirect_to root_url, :alert => "Authentication error: #{params[:message].humanize}"
   end
 
 end
