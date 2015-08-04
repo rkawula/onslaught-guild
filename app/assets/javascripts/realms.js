86615e0cd1ca1ec36dd33b012e5cd1a812606a5d
Rachel Kawula <rkawula@gmail.com>
2015-08-03 21:12:36 -0700

    Updated JavaScript playing with AJAX.

--- app/assets/javascripts/realms.js
+++ app/assets/javascripts/realms.js
@@ -1,9 +1,10 @@
 d3.json("https://us.api.battle.net/wow/realm/status?locale=en_US&apikey=g4z3ngfmwsndpwtzr43e6tbps3crsqb7", function(error, root) {
   if (error) throw error;
-
+  list = []
   root['realms'].forEach(function(realm) {
-  	console.log(realm.name);
+  	list.push(realm.name)
   });
-  
+  window.alert(list);
+  document.getElementById('test').innerHTML = list;
 
 });
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,30 @@
+module SessionsHelper
+
+	GUEST_PERMISSION_LEVEL = 5
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+    def correct_user?
+      @user = User.find(params[:id])
+      unless current_user == @user
+        redirect_to root_url, :alert => "Access denied."
+      end
+    end
+
+    def authenticate_user!
+      if !current_user
+        redirect_to root_url, :alert => 'You need to sign in for access to this page.'
+      end
+    end
+
+    def permission
+    	return logged_in ? current_user.permission_level : GUEST_PERMISSION_LEVEL
+    end
+  
+end
