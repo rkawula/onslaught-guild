104de8d89ce09f7084424e1b45e88cc0f5a408a4
Rachel Kawula <rkawula@gmail.com>
2016-01-27 23:43:38 -0800

    Added forums to the seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,27 @@
 # Raiding (members only)
 # Officers (officers only)
 # Applications (public)
+
+Forum.create name: 'General',
+	description: 'General chat for Onslaught and visitors.',
+	visibility: 5
+
+Forum.create name: 'Applications',
+	description: 'Apply to Onslaught here.',
+	visibility: 5
+
+Forum.create name: 'Guild Hall',
+	description: 'Guild chat.',
+	visibility: 3
+
+Forum.create name: 'Class Discussion',
+	description: 'All classes welcome.',
+	visibility: 3
+
+Forum.create name: 'Raiding',
+	description: 'Updates on guild progress and events.',
+	visibility: 3
+
+Forum.create name: 'Officer Chat',
+	description: 'Secret clubhouse.',
+	visibility: 2
